3d9205becccfe2274fb70b49a3620dec0d5830c6
DanielxSantos <daniel.victorst@gmail.com>
2018-08-01 17:20:27 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -23,8 +23,8 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> buscar(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
 		
+		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 
 	}
