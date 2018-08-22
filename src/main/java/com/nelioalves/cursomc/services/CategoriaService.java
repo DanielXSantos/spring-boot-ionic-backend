fcac9c0604639de0a9cd3b92ba15fbcb8acbb0e9
DanielxSantos <daniel.victorst@gmail.com>
2018-08-22 17:17:05 -0300

    Atualizando Categoria com PUT

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -19,7 +19,7 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	
-	public Categoria buscar(Integer id)
+	public Categoria find(Integer id)
 	{
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -28,6 +28,12 @@ public class CategoriaService {
 	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj)
+	{
+		find(obj.getId());
 		return repo.save(obj);
 	}
 	
